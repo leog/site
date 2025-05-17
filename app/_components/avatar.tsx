7bcf0e607dabc7bdbcf45ccbe4031ac97c58e53f
Leo Giovanetti <hello@leog.me>
2025-05-17 16:03:31 -0300

    fixing top margin on avatar

--- app/_components/avatar.tsx
+++ app/_components/avatar.tsx
@@ -2,7 +2,7 @@ export function Avatar({
   src,
 }: Readonly<{ src: string }>) {
   return (
-    <div className='avatar top-[20%] md:top-[15%] right-0 fade-in'>
+    <div className='avatar top-[13%] md:top-[15%] right-0 fade-in'>
       <img src={src} alt='' />
     </div>
   );
